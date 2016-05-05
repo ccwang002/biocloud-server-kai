5d4ddd44123f9f8825c38744d0151f1e6bee9df2
Liang-Bo Wang <me@liang2.tw>
2016-05-06 02:32:51 +0800

    Add data source model into admin

--- src/data_sources/admin.py
+++ src/data_sources/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
-# Register your models here.
+from .models import DataSource
+
+
+class ChecksumBlankListFilter(admin.SimpleListFilter):
+    title = _('Checksum')
+    parameter_name = 'blank'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('0', _('Blank')),
+            ('1', _('Computed')),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == '0':
+            return queryset.filter(checksum__exact='')
+        if self.value() == '1':
+            return queryset.exclude(checksum__exact='')
+
+
+@admin.register(DataSource)
+class DataSourceAdmin(admin.ModelAdmin):
+
+    fields = [
+        'owner', 'file_path', 'checksum',
+    ]
+    search_fields = ['owner', 'file_path']
+    list_display = [
+        'owner', 'file_path', 'checksum',
+    ]
+    list_filter = (ChecksumBlankListFilter,)
