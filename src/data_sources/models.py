eb6c725f07b2379ab8df638ddbbbe765bf360769
Liang-Bo Wang <me@liang2.tw>
2016-05-06 02:32:23 +0800

    Add data source model w. SHA256 checksum

--- src/data_sources/models.py
+++ src/data_sources/models.py
@@ -1,22 +1,55 @@
+from django import forms
 from django.conf import settings
+from django.core.validators import RegexValidator
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
-# class DataSource(models.Model):
-#     """A model to store user's data sources.
-#
-#     """
-#     owner = models.ForeignKey(
-#         settings.AUTH_USER_MODEL,
-#         on_delete=models.CASCADE,
-#         related_name='data_sources',
-#     )
-#
-#     # Don't use FilePathField, not that useful.
-#     # Only check file existence at forms
-#     file_path = models.CharField(max_length=1023)
-#
-#     sha256sum
-#
-#     is_binary
-#
+class SHA256ChecksumField(models.CharField):
+    default_validators = [
+        RegexValidator(
+            regex=r'^[A-Fa-f0-9]{64}$',
+            message=_('Invalid SHA256 checksum format'),
+            code='invalid_sha256sum',
+        ),
+    ]
+    description = _("SHA256 checksum")
+
+    def __init__(self, blank=False):
+        # SHA-256 checksum in hexadecimal representation takes up 64 chars,
+        # e.g. 4bit * 64 = 256bit
+        super().__init__(blank=blank, max_length=64)
+
+    def deconstruct(self):
+        name, path, args, kwargs = super().deconstruct()
+        del kwargs['max_length']
+        return name, path, args, kwargs
+
+    def formfield(self, **kwargs):
+        return super().formfield(**{
+            **kwargs,
+            'form_class': forms.CharField
+        })
+
+
+class DataSource(models.Model):
+    """A model to store user's data sources.
+
+    """
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='data_sources',
+    )
+
+    # Don't use FilePathField, not that useful.
+    # Only check file existence at forms
+    file_path = models.CharField(max_length=1023)
+
+    checksum = SHA256ChecksumField(blank=True)
+
+    def __str__(self):
+        return (
+            '%s/%s (owner: %s)' %
+            (self.owner.pk, self.file_path, self.owner.name)
+        )
