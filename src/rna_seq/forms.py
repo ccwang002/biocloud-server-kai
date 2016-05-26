25a0d8454fb105fc1a80870cd23b1de4ba0f0ddf
Liang-Bo Wang <me@liang2.tw>
2016-05-27 00:24:34 +0800

    Update RNA-Seq form

--- src/rna_seq/forms.py
+++ src/rna_seq/forms.py
@@ -1,24 +1,42 @@
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Field, Fieldset, Layout, Submit
+from crispy_forms.layout import Field, Fieldset, HTML, Layout, Submit
 from crispy_forms.bootstrap import FormActions
 from django.utils.functional import cached_property
 from django.utils.translation import ugettext_lazy as _
+from django.utils.timezone import now
 
 from pipelines.forms import AbstractPipelineCreateForm
+from pipelines.fields import DataSourceModelChoiceField
 from .models import RNASeqModel
 
 
 class RNASeqCreateForm(AbstractPipelineCreateForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['fastq_sources'].queryset = self._data_sources
+
+    fastq_sources = DataSourceModelChoiceField(
+        label='FASTQ Sources',
+        queryset=None,
+    )
 
     @cached_property
     def helper(self):
         helper = FormHelper()
         helper.layout = Layout(
             Fieldset(
-                '',
+                'Analysis description',
                 Field(
                     'analysis_name',
-                    placeholder=self.fields['analysis_name'].label
+                    value='RNA-Seq {:%Y-%m-%d %H:%M}'.format(now())
+                ),
+            ),
+            Fieldset(
+                'Data Sources',
+                HTML("<p>Specifiy your input FASTQ sources here.</p>"),
+                Field(
+                    'fastq_sources',
                 ),
             ),
             FormActions(
@@ -31,4 +49,7 @@ class RNASeqCreateForm(AbstractPipelineCreateForm):
 
     class Meta:
         model = RNASeqModel
-        fields = ('analysis_name', )
+        fields = (
+            *AbstractPipelineCreateForm._meta.fields,
+            'fastq_sources'
+        )
--- src/rna_seq/models.py
+++ src/rna_seq/models.py
@@ -1,5 +1,11 @@
+from django.db import models
 from pipelines.models import AbstractPipelineModel
+from data_sources.models import DataSource
 
 
 class RNASeqModel(AbstractPipelineModel):
-    pass
+
+    fastq_sources = models.ManyToManyField(
+        DataSource,
+        related_name='+',
+    )
