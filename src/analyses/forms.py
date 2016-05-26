b5be99f0ba6900f1887803cc3df4ae070e44b333
Liang-Bo Wang <me@liang2.tw>
2016-05-26 18:33:07 +0800

    More ChoiceEnum in to core app

--- src/analyses/forms.py
+++ src/analyses/forms.py
@@ -7,27 +7,7 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Field, Fieldset, Layout, Submit, HTML
 from crispy_forms.bootstrap import FormActions
 
-
-class ChoiceEnum(enum.Enum):
-
-    @classmethod
-    def choices(cls):
-        return [
-            (member_name, member.value)
-            for member_name, member in cls.__members__.items()
-        ]
-
-    @classmethod
-    def from_choice(cls, choice):
-        try:
-            return cls.__members__[choice]
-        except KeyError as e:
-            raise ValueError(
-                '%(cls)s does not have the given choice %(choice)r.' % {
-                    'cls': cls.__name__,
-                    'choice': choice,
-                }
-            ) from e
+from core.choices import ChoiceEnum
 
 
 class AnalysisType(ChoiceEnum):
