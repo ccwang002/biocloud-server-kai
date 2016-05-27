10494456af67270f28853872dd8072f01872b6db
Liang-Bo Wang <me@liang2.tw>
2016-05-27 09:41:34 +0800

    Add message after successfully created an new analysis

--- src/pipelines/views.py
+++ src/pipelines/views.py
@@ -1,4 +1,6 @@
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils.translation import ugettext_lazy as _
 from django.views.generic import CreateView
 
 from .forms import AbstractPipelineCreateForm
@@ -14,3 +16,14 @@ class AbstractPipelineFormView(LoginRequiredMixin, CreateView):
         kwargs = super().get_form_kwargs()
         kwargs['request'] = self.request
         return kwargs
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.add_message(
+            self.request, messages.INFO,
+            _('You just created a %(analysis_type)s analysis!') % {
+                'analysis_type': self.object.analysis_type
+            }
+        )
+        return response
+
