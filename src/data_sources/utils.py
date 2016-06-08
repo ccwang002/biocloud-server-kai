daebd0e2f1fe4a02ac61a509bacc56e13293a068
Liang-Bo Wang <me@liang2.tw>
2016-06-08 19:51:44 +0800

    Update comment

--- src/data_sources/utils.py
+++ src/data_sources/utils.py
@@ -3,6 +3,17 @@ import re
 
 
 def guess_data_source(file_path):
+    """Guess data source info based on file path.
+
+    It first detects the file suffix and delegates to the proper
+    data source info extractor.
+
+    Args:
+        file_path (str): file path to the data source
+
+    Returns:
+        dict: initial data to create the DataSource object
+    """
     pth = Path(file_path)
     initial = {
         'file_path': file_path,
@@ -19,20 +30,21 @@ def guess_data_source(file_path):
 
 
 def complete_fastaq_info(file_path, initial):
-    """Guess sample name from file path
+    """Guess FASTA/Q data source info based on file path.
 
     Args:
         file_path (str): file path to the data source
         initial (dict): initial data
 
-    Returns: dict
+    Returns:
+        dict: initial data to create the DataSource object
     """
     pth = Path(file_path)
     initial = initial.copy()
     sample_regex = (
-        r'^(?P<sample>\w+)_'     # sample name
+        r'^(?P<sample>\w+)_'       # sample name
         r'[rR]?(?P<strand>[12])$'  # read 1 or read 2 (pair-end)
-                                 # can be R1 / R2
+                                   # can be R1 / R2
     )
     match = re.match(sample_regex, pth.stem)
     if match:
