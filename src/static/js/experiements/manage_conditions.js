e64d822ff7a490650350988c28cffeeb3f7caee1
Liang-Bo Wang <me@liang2.tw>
2016-06-10 01:24:04 +0800

    An interactive condition designer based on Vue.js

--- src/static/js/experiements/manage_conditions.js
+++ src/static/js/experiements/manage_conditions.js
@@ -1,21 +1,60 @@
-new Vue({
-    el: '#conditions',
+var vm = new Vue({
+    el: '#app',
     data: {
-        newCondition: '',
+        editable: true,
         conditions: [
-            // { text: 'Add new conditions' }
-        ]
+        ],
+        numConditionCreated: 0,
+        sourceFilter: '',
+        dataSources: data_sources
+    },
+    computed: {
+        filteredDataSources: function() {
+            var filterStr = this.sourceFilter;
+            return this.dataSources.filter(function (source) {
+                return source.file_path.indexOf(filterStr) > -1;
+            });
+        },
+        selectedDataSources: function() {
+            var filterStr = this.sourceFilter;
+            return this.dataSources.filter(function (source) {
+                return source.selected;
+            });
+        }
     },
     methods: {
-        addCondition: function () {
-            var text = this.newCondition.trim();
-            if (text) {
-                this.conditions.push({ text: text });
-                this.newCondition = '';
-            }
-        },
-        removeCondition: function (index) {
+        removeCondition: function (index, event){
+            // remove current condition
             this.conditions.splice(index, 1);
+        },
+        addCondition: function (label) {
+            var defaultLabel = 'New condition ' + (this.numConditionCreated + 1);
+            this.conditions.push({
+                '_uid': this.numConditionCreated,
+                'label': label ? label.trim() : defaultLabel
+            });
+            this.numConditionCreated += 1;
+        },
+        leaveEditMode: function (){
+            // trim leading and trailing spaces of all conditions
+            this.conditions.forEach(function (curVal) {
+                curVal.label = curVal.label.trim();
+            });
+            this.editable = false;
+        },
+        enterEditMode: function() {
+            this.editable = true;
+        },
+        alterFilteredSourcesSelectedStatus: function(status) {
+            // Alter the filtered data sources select status
+            // based on the selectedStatus
+            this.filteredDataSources.forEach(function (source) {
+                source.selected = status;
+            });
         }
     }
-})
+});
+
+// Add two default conditions
+vm.addCondition('Control');
+vm.addCondition('Test');
