c585ca74dd42b168460fd6017ee4acc4e502252e
Liang-Bo Wang <me@liang2.tw>
2016-06-12 23:36:21 +0800

    Expose django admin link to frontend

--- src/dashboard/urls.py
+++ src/dashboard/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from .views import dashboard_home, dashboard_profile_update
+from .views import dashboard_home, dashboard_profile_update, DashboardAdminView
 
 
 urlpatterns = [
     url(r'^$', dashboard_home, name='dashboard_home'),
     url(r'^profile/$', dashboard_profile_update, name='dashboard_profile'),
+    url(r'^admin/$', DashboardAdminView.as_view(), name='dashboard_admin'),
 ]
--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -1,8 +1,10 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, redirect
 from django.utils.translation import ugettext
+from django.views.generic import TemplateView
 
 from users.forms import UserProfileUpdateForm
 
@@ -35,5 +37,11 @@ def dashboard_profile_update(request):
         'form': form, 'logout_next': logout_next,
     })
 
+
+class DashboardAdminView(PermissionRequiredMixin, TemplateView):
+    permission_required = 'is_staff'
+    template_name = 'dashboard/admin.html'
+    raise_exception = True
+    permission_denied_message = 'User of current session is not an admin'
 
 
