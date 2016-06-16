0502fc6756e7e32b1684d3e5edb21c445dc989a2
Liang-Bo Wang <me@liang2.tw>
2016-06-17 01:16:41 +0800

    Add genome reference model

--- src/analyses/admin.py
+++ src/analyses/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
-# Register your models here.
+from .models import GenomeReference
+
+
+@admin.register(GenomeReference)
+class GenomeReferenceAdmin(admin.ModelAdmin):
+
+    fields = (
+        'organism',
+        'identifier',
+        'source',
+        'newer_reference',
+    )
+    list_display = (
+        'organism',
+        'identifier',
+        'source',
+        'newer_reference',
+    )
+    ordering = (
+        'organism', '-identifier'
+    )
+    list_display_links = ['identifier']
+    list_filter = (
+        'organism', 'source',
+    )
--- src/analyses/models.py
+++ src/analyses/models.py
@@ -15,6 +15,54 @@ class ExecutionStatus(ChoiceEnum):
     FAILED = _("Failed")
 
 
+class GenomeReference(models.Model):
+
+    identifier = models.CharField(
+        primary_key=True,
+        max_length=64,
+        help_text=_(
+            "Identifier for the genome reference. Ex. hg19, GRCh38.p5."
+        ),
+    )
+
+    SOURCE_CHOICES = [
+        ("NCBI", _("NCBI")),
+        ("ENSEMBL", _("Ensembl")),
+        ("UCSC", _("UCSC")),
+        ("OTHER", _("Other or Custom"))
+    ]
+    source = models.CharField(
+        max_length=16,
+        choices=SOURCE_CHOICES,
+        help_text=_(
+            "The institution that maintains the genome reference."
+        ),
+    )
+
+    organism = models.CharField(
+        max_length=512,
+        help_text=_(
+            "Species description using the Linnaean taxonomy naming system. "
+            "Ex. Homo sapiens; Mus musculus."
+        ),
+    )
+
+    newer_reference = models.ForeignKey(
+        "self",
+        blank=True, null=True,
+        on_delete=models.SET_NULL,
+        verbose_name=_("newer version"),
+        help_text=_(
+            "If new version of the genome reference of the same "
+            "organism and source exists, link the reference here. "
+            "For example, UCSC mm9 can set its newer reference to UCSC mm10."
+        ),
+    )
+
+    def __str__(self):
+        return "{0.identifier} ({0.source})".format(self)
+
+
 class AbstractAnalysisModel(models.Model):
 
     owner = models.ForeignKey(
