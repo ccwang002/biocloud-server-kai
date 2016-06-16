3d088809dd4f288ddce81ffa20202c393d6f9363
Liang-Bo Wang <me@liang2.tw>
2016-06-17 01:19:42 +0800

    Add ToolSpec general toolspec finding by name and link formatting

--- src/analyses/tool_specs.py
+++ src/analyses/tool_specs.py
@@ -1,6 +1,7 @@
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
 
 from django.utils.functional import cached_property
+from django.utils.html import mark_safe
 
 
 class ToolSpec:
@@ -37,6 +38,13 @@ class ToolSpec:
     def db_name(self):
         return "%s_%s" % (self.name, self.version)
 
+    def as_link(self):
+        """Format as a HTML link element."""
+        return mark_safe(
+            '<a href="{url:s}" target="_blank">{name:s}</a>'
+            .format(url=self.url, name=self.name)
+        )
+
     def __str__(self):
         return "{name:s} v{ver:s}".format(
             name=self.name, ver=self.version
@@ -56,6 +64,7 @@ class ToolSpec:
 class ToolSet:
     def __init__(self, toolspecs):
         self.tools = OrderedDict()
+        self.general_tools = defaultdict(list)
         for toolspec in toolspecs:
             self.add_tool(toolspec)
 
@@ -76,6 +85,7 @@ class ToolSet:
                 .format(toolspec=toolspec)
             )
         self.tools[toolspec.db_name] = toolspec
+        self.general_tools[toolspec.name].append(toolspec)
 
     @cached_property
     def db_choices(self):
@@ -84,12 +94,18 @@ class ToolSet:
             for db_name, toolspec in self.tools.items()
         ]
 
+    @cached_property
+    def each_per_name(self):
+        return {
+            name: toolspecs[0]
+            for name, toolspecs in self.general_tools.items()
+        }
+
     def subset(self, tool_names):
         """Return a new ToolSet leaving only tools within tool_names.
 
         """
-        return ToolSet([
-            toolspec
-            for toolspec in self.tools.values()
-            if toolspec.name in tool_names
-        ])
+        toolspecs = []
+        for name in tool_names:
+            toolspecs.extend(self.general_tools[name])
+        return ToolSet(toolspecs)
